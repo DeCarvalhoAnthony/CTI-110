dec381d85ea0d895eeb0e4d5c372d9ba2337a8c1
DeCarvalhoAnthony <43179013+DeCarvalhoAnthony@users.noreply.github.com>
2018-09-11 11:51:34 -0400

    Add files via upload
    Submission of Python Programing P2T1.

---
+++ P2T1_SalesPrediction_DeCarvalhoAnthony.py
@@ -0,0 +1,13 @@
+# Get the projected total sales.
+ # 11 Sep 2018
+ # CTI-110 P2T1 - Sales Prediction
+ # Anthony DeCarvalho
+ #
+total_sales = float(input('Enter the projected sales: '))
+
+#Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+#Display the profit.
+print('The profit $', format(profit, ',.2f'))
+
